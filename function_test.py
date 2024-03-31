f2f365b5b64c95a75e5b42e2065071a336b411d1
TheNorthRem <1371002466@qq.com>
2024-03-31 15:24:14 +0800

    message 6 : functional test now checks we can input a to-do item

--- function_test.py
+++ function_test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 import unittest
-
+import time
 
 class NewVistorTest(unittest.TestCase):
 	def setUp(self):
@@ -15,14 +17,30 @@ class NewVistorTest(unittest.TestCase):
 
 		self.assertIn('To-Do',self.browser.title), "broswer title was " + self.browser.title
 
+		header_test = self.browser.find_element(By.TAG_NAME,'h1').text
+
+		self.assertIn('To-Do',header_test)
+
+		input_box = self.browser.find_element(By.ID,'id_new_item')
+
+		self.assertEqual(input_box.get_attribute('placeholder'),
+				   'Enter a to-do item')
+		
+		input_box.send_keys('Buy flowers')
+
+		input_box.send_keys(Keys.Enter)
+
+		time.sleep(1)
+
+		table = self.browser.find_element(By.ID,'id_list_table')
+		rows = table.find_element(By.TAG_NAME,'tr')
+
+		self.assertIn('1: Buy flowers', [row.text for row in rows])
+
+	
+
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
 	unittest.main()
 
-
-# broswer = webdriver.Chrome()
-
-# broswer.get('http://localhost:8000')
-
-# assert 'T' in broswer.page_source
