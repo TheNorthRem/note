2bf3bd46d0cdb3b4059223d9fc1c80f181a7e696
TheNorthRem <1371002466@qq.com>
2024-03-31 14:12:41 +0800

    func_test update

--- function_test.py
+++ function_test.py
@@ -1,7 +1,28 @@
 from selenium import webdriver
 
-broswer = webdriver.Chrome()
+import unittest
 
-broswer.get('http://localhost:8000')
 
-assert 'Django' in broswer.page_source
+class NewVistorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('To-Do',self.browser.title), "broswer title was " + self.browser.title
+
+		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+# broswer = webdriver.Chrome()
+
+# broswer.get('http://localhost:8000')
+
+# assert 'T' in broswer.page_source
