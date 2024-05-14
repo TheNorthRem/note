806b80d159b950f4fe9b15be3f13494c3feb7dd3
TheNorthRem <1371002466@qq.com>
2024-05-14 21:56:50 +0800

    message 16 : from URL of new list by POST

--- lists/views.py
+++ lists/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from lists.models import Item
 # Create your views here.
-
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-new-page/')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html',{'items':items})
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text =  request.POST['item_text'])
-        return redirect('/lists/the-new-page/')
     return render(request,'home.html')
 
